a506e1657c07583a0d4cf6335724831cb52fd317
hugh_chao <hugh.chao@dell.com>
2025-08-28 19:48:26 +0800

    Save progress

---
+++ stress/hooks/002-check-wifi.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+target_device=$(cat ~/.stress_config/target_device)
+device=$(ip a | grep "$target_device")
+log_base=$(basename $0)
+err_file=${log_base%".sh"}".err"
+count_file=~/.stress_config/"$err_file"
+current_error=~/.stress_config/this_error
+
+if [ -z "$count_file" ]; then
+	echo 0 > "$count_file"
+fi
+count_error=$(cat $count_file)
+
+err_m=$(sudo dmesg | grep "iwlwifi" | grep -i "failed")
+
+if [ -n "$err_m "]; then
+	count_error=$((count_error + 1))
+  echo $count_error > $count_file
+	echo "Detect Wifi error message - $err_m" >> "$current_error"
+	exit -1
+fi
--- stress/setup-stress.sh
+++ stress/setup-stress.sh
@@ -9,8 +9,8 @@ fi
 chmod +x ./bin/*
 ./bin/env-setup.sh
 if [ $? -ne 0 ]; then
-        echo '\033[0;31mError: ENV setup failed\033[0m'
-        exit 1
+		echo '\033[0;31mError: ENV setup failed\033[0m'
+		exit 1
 fi
 
 user=$(whoami)
@@ -18,13 +18,12 @@ STRESS_COUNT=50
 TARGET_DEVICE=enx6018956e2b29
 
 if [ ! -d ~/.stress_config ]; then
-        mkdir ~/.stress_config
+    mkdir ~/.stress_config
 fi
 
 echo "$STRESS_COUNT" >  ~/.stress_config/count_reboot
 echo "$STRESS_COUNT" >  ~/.stress_config/count_reboot_total
 echo "reboot" >  ~/.stress_config/method
-echo 0 > ~/.stress_config/count_error
 echo 0 > ~/.stress_config/err_stop
 echo "$TARGET_DEVICE" > ~/.stress_config/target_device
 
@@ -53,69 +52,79 @@ EOF
 sudo sed -i s/"User=THE_USER"/"User=$user"/g /etc/systemd/system/shutdown_stress.service
 sudo systemctl enable shutdown_stress.service
 
+sudo mkdir -p /usr/local/stress_hook/
+sudo cp bin/* /usr/local/stress_hook/
+sudo chmod 744 /usr/local/stress_hook/*.sh
+
 #setup runtime script
 sudo bash -c "cat >/usr/bin/run_shutdown_stress" <<"EOF"
 #!/bin/bash
-count_file=~/.stress_config/count_reboot
+
+#File Pathes
 count_file_total=~/.stress_config/count_reboot_total
-count_file_error=~/.stress_config/count_error
-count_file_log=~/.stress_config/error_log
 err_stop_file=~/.stress_config/err_stop
+current_error=~/.stress_config/this_error
+stress_hooks=/usr/local/stress_hook/
+count_file=~/.stress_config/count_reboot
+log_file=~/.stress_config/logs
+
+# Variables
+method=$(cat ~/.stress_config/method)
 count=$(cat $count_file)
 count_total=$(cat $count_file_total)
-count_error=$(cat $count_file_error)
 err_stop=$(cat $err_stop_file)
+errors=$(cat "$current_error")
 do_stop=0
 output_message=""
 #-1=detected
 #0 =do no
 #1 =shutown
 service_status=0 # 0=do nothing, 
-target_device=$(cat ~/.stress_config/target_device)
 STRESS_BOOT_WAKEUP_DELAY=60
 
-device=$(ip a | grep "$target_device")
-#err_m=$(sudo dmesg | grep "iwlwifi" | grep -i "failed")
-err_m=$(sudo dmesg | grep "Bluetooth" | grep -i "fail")
-method=$(cat ~/.stress_config/method)
+# Initialize
+echo "" > $current_error
+echo "" > $log_file
 
-if [ ! "$count" -gt 0 ]; then
-        #Show Report and exit
-
-        if [[ -n "$err_m" || -z "$device" ]]; then
-                count_error=$((count_error + 1))
-                echo $count_error > $count_file_error
-        fi
-        output_message="Finished stress $count_total times, detected $count_error times"
-	notify-send "Info" "$output_message"
-        sudo systemctl disable shutdown_stress.service
-        sudo systemctl stop shutdown_stress.service
-        exit 0
-elif [[ -n "$err_m" || -z "$device" ]]; then
-        service_status=-1
-        count_error=$((count_error + 1))
-	output_message="Err detected! $err_m \n "
-	echo "$err_m" >> "$count_file_log"
-	if [ -z "$device" ]; then
-		echo "Can not find $device" >> "$count_file_log"
-		output_message="$output_message, \nCan not find $device "
-	fi
-        echo $count_error > $count_file_error
-
-	if [ "$err_stop" == 1 ]; then
+for file in "$stress_hooks"/*.sh; do
+	bash $file
+	
+	if [[ "$?" != 0 && "$err_stop" == 1 ]]; then
 		do_stop=1
 	fi
+done
+
+if [ ! "$count" -gt 0 ]; then
+    #Show Report and exit
+
+    output_message="Finished stress $count_total times, detected error"
+		notify-send "Info" "$output_message"
+    sudo systemctl disable shutdown_stress.service
+    sudo systemctl stop shutdown_stress.service
+    exit 0
+elif [ -n "$errors" ]; then
+		# Got errors
+		
+		service_status=-1
+		output_message="Err detected! $errors \n "
+		echo "" >> "$log_file"
+		echo "$======$count======" >> "$log_file"
+		echo "$errors" >> "$log_file"
+
+		if [ "$err_stop" == 1 ]; then
+				do_stop=1
+		fi
 else
-        output_message="$method stress ($count/$count_total), will $method soon "
-        service_status=1
+    output_message="$method stress ($count/$count_total), will $method soon "
+    service_status=1
 fi
 
 count=$((count - 1))
 echo $count > $count_file
 
 if [ "$service_status" == 1 ]; then
-	notify-send "Info" "$output_message"
-        sleep 10
+		notify-send "Info" "$output_message"
+    sleep 10
 elif [ "$service_status" == -1 ]; then
 	notify-send "Warning" "$output_message"
 fi
